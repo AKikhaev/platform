6f0471ead441233f812138235bfc011e6b878cc6
AKikhaev <aliday.pr@gmail.com>
2018-07-23 12:11:56 +0300

    project_name variable to build config #1

--- server/monitor.sh
+++ server/monitor.sh
@@ -1,6 +1,6 @@
 name=knpz-ken.ru
-nameShort=`echo $name | sed 's/[ \t\.-]//g'`
-
-touch /data/nfs/$name/logs/terminal && chmod 666 /data/nfs/$name/logs/terminal && truncate --size 0 /data/nfs/$name/logs/terminal
-trap "rm /data/nfs/$name/logs/terminal" EXIT INT TERM
-tail -f /data/nfs/$name/logs/terminal
+nameShort=`echo $name | sed 's/[ \t\.-]/_/g'`
+echo $nameShort
+#touch /data/nfs/$name/logs/terminal && chmod 666 /data/nfs/$name/logs/terminal && truncate --size 0 /data/nfs/$name/logs/terminal
+#trap "rm /data/nfs/$name/logs/terminal" EXIT INT TERM
+#tail -f /data/nfs/$name/logs/terminal
